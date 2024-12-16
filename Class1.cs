16fd199223305aeaf85279da97f89195bd81483b
KATYANDR <150719669+KATYANDR@users.noreply.github.com>
2024-12-16 18:26:57 +0300

    update

---
+++ Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptionLibrary
+{
+    public static class Encryptor
+    {
+        public static string EncryptString(string plainText, string salt)
+        {
+            byte[] key = Encoding.UTF8.GetBytes(salt.PadRight(32));
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.Mode = CipherMode.CBC;
+                aesAlg.Padding = PaddingMode.PKCS7;
+
+                aesAlg.GenerateIV();
+                byte[] iv = aesAlg.IV;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, iv);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    msEncrypt.Write(iv, 0, iv.Length);
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string DecryptString(string cipherText, string salt)
+        {
+            byte[] fullCipher = Convert.FromBase64String(cipherText);
+
+            byte[] iv = new byte[16];
+            Array.Copy(fullCipher, 0, iv, 0, iv.Length);
+
+            byte[] cipher = new byte[fullCipher.Length - iv.Length];
+            Array.Copy(fullCipher, iv.Length, cipher, 0, cipher.Length);
+
+            byte[] key = Encoding.UTF8.GetBytes(salt.PadRight(32));
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+                aesAlg.Mode = CipherMode.CBC;
+                aesAlg.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(cipher))
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                {
+                    return srDecrypt.ReadToEnd();
+                }
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using EncryptionLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class ResultTxt : Form
+    {
+        public ResultTxt()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string plainText = txtPlainText.Text;
+            string salt = txtSalt.Text;
+
+            if (string.IsNullOrEmpty(plainText) || string.IsNullOrEmpty(salt))
+            {
+                MessageBox.Show("Пожалуйста, введите текст и соль.");
+                return;
+            }
+
+            string encryptedText = Encryptor.EncryptString(plainText, salt);
+            txtResult.Text = encryptedText;
+        }
+
+    private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            string cipherText = txtResult.Text;
+            string salt = txtSalt.Text;
+
+            if (string.IsNullOrEmpty(cipherText) || string.IsNullOrEmpty(salt))
+            {
+                MessageBox.Show("Пожалуйста, введите зашифрованный текст и соль.");
+                return;
+            }
+
+            try
+            {
+                string decryptedText = Encryptor.DecryptString(cipherText, salt);
+                txtPlainText.Text = decryptedText;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при расшифровке: " + ex.Message);
+            }
+        }
+
+    }
+}
